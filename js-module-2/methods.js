1f6492beb6e04833e82688fdfe60e5cc2434f731
Svit-l <lov.svit@gmail.com>
2021-09-12 00:26:06 +0300

    Закрепление методов

---
+++ js-module-2/methods.js
@@ -0,0 +1,19 @@
+const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+console.log(clients.includes("Poly"));
+
+console.log(clients.push("Zina", "Lada", "Geva"));
+console.log(clients);
+
+const newArray = [...clients, ...numbers];
+console.log(newArray);
+
+// const clientsAdd = clients.pop();
+// console.log(clientsAdd);
+// clients.length = 5;
+// console.log(clients);
+
+// console.log(clients.join(" "));
+
+console.log(newArray.unshift("Doly", 3, 5, "nady"));
+console.log(newArray);
