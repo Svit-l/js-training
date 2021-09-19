08a89eb689cd728a33744241baffaf2049f6fe75
Svit-l <lov.svit@gmail.com>
2021-09-19 04:18:38 +0300

    Задачи 30-41 Модуль3

--- js-module-3/task-03-30.js
+++ js-module-3/task-03-30.js
@@ -5,9 +5,11 @@
 // text - текст задачи.
 // category - категория задачи.
 // priority - приоритет задачи.
-// Функция должна составить и вернуть новый объект задачи, не изменяя напрямую параметр data. В новом объекте должно быть свойство completed, значение которого хранится в одноимённой локальной переменной.
+// Функция должна составить и вернуть новый объект задачи, не изменяя напрямую параметр data.
+// В новом объекте должно быть свойство completed, значение которого хранится в одноимённой локальной переменной.
 
-// В параметре data гарантированно будет только свойство text, а остальные два, category и priority, могут отсутствовать. Тогда, в новом объекте задачи, в свойствах category и priority должны быть значения по умолчанию, хранящиеся в одноимённых локальных переменных.
+// В параметре data гарантированно будет только свойство text, а остальные два, category и priority, могут отсутствовать.
+// Тогда, в новом объекте задачи, в свойствах category и priority должны быть значения по умолчанию, хранящиеся в одноимённых локальных переменных.
 
 // Тесты
 // Объявлена функция makeTask(data)
@@ -18,12 +20,28 @@
 // Вызов makeTask({ text: "Buy bread" }) возвращает { category: "General", priority: "Normal", text: "Buy bread", completed: false }
 
 function makeTask(data) {
+  // console.log(data);
   const completed = false;
   const category = "General";
   const priority = "Normal";
   // Change code below this line
-
+  const dataNew = {
+    category,
+    priority,
+    completed,
+    ...data,
+  };
+  console.log(dataNew);
+  return dataNew;
   // Change code above this line
 }
 
-// console.log(number);
+makeTask({});
+
+makeTask({
+  category: "Homemade",
+  priority: "Low",
+  text: "Take out the trash",
+});
+
+makeTask({ text: "Buy bread" });
--- js-module-3/task-03-32.js
+++ js-module-3/task-03-32.js
@@ -1,34 +1,49 @@
-// Задача: функция includes()
-// Задание
-// Напиши функцию includes(array, value), которая делает тоже самое, что и метод массива массив.includes(значение) -
-// проверяет, есть ли в массиве array значение value, возвращая true если есть и false в противном случае.
+// Операция rest для сбора части аргументов функции
+// Операция ... (rest) также позволяет собрать в массив только ту часть аргументов,
+// которая необходима, объявив параметры до «сбора».
 
-// При выполнении этой задачи в теле функции includes() нельзя использовать метод массив.includes(значение).
+function multiply(firstNumber, secondNumber, ...otherArgs) {
+  console.log(firstNumber); // Значение первого аргумента
+  console.log(secondNumber); // Значение второго аргумента
+  console.log(otherArgs); // Массив остальных аргументов
+}
+
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4);
+
+// Все аргументы, для которых будут объявлены параметры, передадут им свои значения,
+// остальные аргументы будут помещены в массив.
+// Операция rest собирает все оставшиеся аргументы и поэтому должна идти последней в подписи функции,
+// иначе будет ошибка.
+
+// Задание
+// Функция addOverNum() считает сумму всех аргументов.
+// Измени параметры и тело функции addOverNum() так,
+//   чтобы она считала сумму только тех аргументов, которые больше чем заданное число.
+//   Это число должно быть первым параметром функции.
 
 // Тесты
-// Объявлена функция includes(array, value)
-// Вызов includes([1, 2, 3, 4, 5], 3) возвращает true
-// Вызов includes([1, 2, 3, 4, 5], 17) возвращает false
-// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter") возвращает true
-// Вызов includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus") возвращает false
-// Вызов includes(["apple", "plum", "pear", "orange"], "plum") возвращает true
-// Вызов includes(["apple", "plum", "pear", "orange"], "kiwi") возвращает false
-// Вызов includes() для случайного массива со случайным value возвращает верный boolean
-// В функции includes используется for, return, но не метод массива includes
+// Объявлена функция addOverNum()
+// Вызов addOverNum(50, 15, 27) возвращает 0
+// Вызов addOverNum(10, 12, 4, 11, 48, 10, 8) возвращает 71
+// Вызов addOverNum(15, 32, 6, 13, 19, 8) возвращает 51
+// Вызов addOverNum(20, 74, 11, 62, 46, 12, 36) возвращает 218
 
-function includes(array, value) {
-  // Change code below this line
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return true;
+// Change code below this line
+function addOverNum(minNumber, ...args) {
+  let total = 0;
+
+  for (const arg of args) {
+    if (arg > minNumber) {
+      total += arg;
     }
   }
-  return false;
+
+  return total;
   // Change code above this line
 }
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(
-  includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter")
-);
-console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+console.log(addOverNum(50, 15, 27));
+console.log(addOverNum(10, 12, 4, 11, 48, 10, 8));
+console.log(addOverNum(20, 74, 11, 62, 46, 12, 36));
