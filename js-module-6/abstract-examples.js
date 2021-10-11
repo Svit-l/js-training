a362f0ddbfc29f2fd104e996b9e044499877502f
Svit-l <lov.svit@gmail.com>
2021-10-11 23:11:48 +0300

    модуль6 -конспект продолжение

--- js-module-6/abstract-examples.js
+++ js-module-6/abstract-examples.js
@@ -19,20 +19,42 @@
 // console.log(document.querySelectorAll("li"));
 
 // +++++++++++++++++Abstract-example-2
-const listWithId = document.querySelector("#menu");
-listWithId.style.textTransform = "uppercase";
-listWithId.style.fontSize = "24px";
-console.log(listWithId);
+// const listWithId = document.querySelector("#menu");
+// listWithId.style.textTransform = "uppercase";
+// listWithId.style.fontSize = "24px";
+// console.log(listWithId);
 
-const listWithClass = document.querySelector(".menu");
-console.log(listWithClass);
+// const listWithClass = document.querySelector(".menu");
+// console.log(listWithClass);
 
-const menuItemsByTagName = document.querySelectorAll("li");
-console.log(menuItemsByTagName);
+// const menuItemsByTagName = document.querySelectorAll("li");
+// console.log(menuItemsByTagName);
 
-const menuItemsByClass = document.querySelectorAll(".menu-item");
-console.log(menuItemsByClass);
+// const menuItemsByClass = document.querySelectorAll(".menu-item");
+// console.log(menuItemsByClass);
 
-const firstMenuItem = document.querySelector(".menu-item");
-firstMenuItem.style.color = "tomato";
-console.log(firstMenuItem);
+// const firstMenuItem = document.querySelector(".menu-item");
+// firstMenuItem.style.color = "tomato";
+// console.log(firstMenuItem);
+
+// +++++++++++++++++Abstract-example-3
+//===================================================
+// const message = document.querySelector("#message");
+// console.log(message.value); // Default textarea message
+
+// //===================================================
+// const activeLink = document.querySelector(".btn.active");
+// console.log(activeLink.href); // https://s.codepen.io/about
+
+// //===================================================
+// const image = document.querySelector(".image");
+// console.log(image.src); // https://placeimg.com/640/480/animals
+// image.src = "https://placeimg.com/640/480/tech";
+
+// +++++++++++++++++Abstract-example-4
+const text = document.querySelector(".article-text");
+console.log(text.textContent); // text inside p.article-text
+
+const title = document.querySelector(".article-title");
+title.textContent = "Welcome to Bahamas!";
+console.log(title.textContent);
