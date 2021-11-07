b30e1ba485e7abf279950877dd9cb168faf4658d
Svit-l <lov.svit@gmail.com>
2021-11-07 23:01:15 +0200

    Промисы

--- js-module-09-Asinh/module-09-Asinh.js
+++ js-module-09-Asinh/module-09-Asinh.js
@@ -28,17 +28,78 @@
 //   console.log(`Interval with id ${timerId} has stopped!`);
 // });
 
-// ==============Дата и время
-const date = new Date();
+// // ==============Дата и время
+// const date = new Date();
 
-console.log(date);
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+// console.log(date);
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
 
-console.log(date.toString());
-// "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
+// console.log(date.toString());
+// // "Fri Jun 18 2021 15:01:35 GMT+0300 (Eastern European Summer Time)"
 
-console.log(new Date(0));
-// "Thu Jan 01 1970 03:00:00 GMT+0300 (Eastern European Standard Time)"
+// console.log(new Date(0));
+// // "Thu Jan 01 1970 03:00:00 GMT+0300 (Eastern European Standard Time)"
 
-console.log(new Date(15000));
-// "Thu Jan 01 1970 03:00:15 GMT+0300 (Eastern European Standard Time)"
+// console.log(new Date(15000));
+// // "Thu Jan 01 1970 03:00:15 GMT+0300 (Eastern European Standard Time)"
+
+// const dateNow = new Date();
+// console.log(dateNow.getTime()); // 1624021654154
+
+// const timeInMs = Date.now();
+// console.log(timeInMs);
+// console.log(dateNow === timeInMs);
+// console.log(dateNow.getTime() === timeInMs); // true
+// Change value of isSuccess variable to call resolve or reject
+
+// +++++++++++++++++++Промисы
+
+// const isSuccess = true;
+
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve("Success! Value passed to resolve function");
+//     } else {
+//       reject("Error! Error passed to reject function");
+//     }
+//   }, 2000);
+// });
+
+// // Will run first
+// console.log("Before promise.then()");
+
+// // Registering promise callbacks
+// promise.then(
+//   // onResolve will run third or not at all
+//   (value) => {
+//     console.log("onResolve call inside promise.then()");
+//     console.log(value); // "Success! Value passed to resolve function"
+//   },
+//   // onReject will run third or not at all
+//   (error) => {
+//     console.log("onReject call inside promise.then()");
+//     console.log(error); // "Error! Error passed to reject function"
+//   }
+// );
+
+// // Will run second
+// console.log("After promise.then()");
+
+// =========== Промис с лекции
+const promise = new Promise((resolve, reject) => {
+  const canFulfill = Math.random() > 0.5;
+
+  setTimeout(() => {
+    if (canFulfill) {
+      resolve("OK");
+    }
+    reject("Not OK");
+  }, 2000);
+});
+
+console.log(promise);
+
+promise.then((result) => {
+  console.log(result);
+});
